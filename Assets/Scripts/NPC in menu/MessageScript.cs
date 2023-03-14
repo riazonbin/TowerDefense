4a242db900147f44d04b0f1fdab6c43047aa3f25
riazonbin <riazonbin@mail.ru>
2023-03-14 21:43:14 +0300

    still adding windows

---
+++ Assets/Scripts/NPC in menu/MessageScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MessageScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    Transform npcMessage;
+
+    //Detect if the Cursor starts to pass over the GameObject
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        //Output to console the GameObject's name and the following message
+        npcMessage.gameObject.SetActive(true);
+    }
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        //Output the following message with the GameObject's name
+        npcMessage.gameObject.SetActive(false);
+    }
+
+
+}
