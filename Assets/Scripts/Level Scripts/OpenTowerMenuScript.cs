bc9010166b503371e8c0b6c5ca438a89a26e25e9
riazonbin <riazonbin@mail.ru>
2023-03-16 12:04:17 +0300

    now adding tower is possible

---
+++ Assets/Scripts/Level Scripts/OpenTowerMenuScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class OpenTowerMenuScript : MonoBehaviour
+{
+    [SerializeField]
+    Transform menuPrefab;
+
+    Transform menuContainer;
+    Vector3 menuPosition;
+
+    [DoNotSerialize]
+    public Vector3 dotPosition;
+
+    [DoNotSerialize]
+    public Transform towerPlaceForPoint;
+
+    private void Start()
+    {
+        dotPosition = transform.position;
+
+        menuContainer = transform.parent.parent.parent.GetComponentsInChildren<Transform>()
+            .FirstOrDefault(x => x.name == "MenuContainer");
+
+        menuPosition = transform.parent.GetComponentsInChildren<Transform>()
+            .FirstOrDefault(x => x.name == "MenuPlaceForPoint").position;
+
+        towerPlaceForPoint = transform.parent.GetComponentsInChildren<Transform>()
+            .FirstOrDefault(x => x.name == "TowerPlaceForPoint");
+    }
+
+
+    public void OpenTowerMenu()
+    {
+        while (menuContainer.childCount > 0)
+        {
+            DestroyImmediate(menuContainer.GetChild(0).gameObject);
+        }
+
+        var menu = Instantiate(menuPrefab, menuContainer);
+
+
+        var menuItemsScripts = menu.GetComponentsInChildren<SelectTowerFromMenuScript>();
+
+        foreach( var item in menuItemsScripts )
+        {
+            item.openTowerMenuScript = this;
+        }
+
+        menu.position = menuPosition;
+    }
+}
---
+++ Assets/Scripts/Level Scripts/SelectTowerFromMenuScript.cs
@@ -0,0 +1,41 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
+
+public class SelectTowerFromMenuScript : MonoBehaviour
+{
+    [System.NonSerialized]
+    public OpenTowerMenuScript openTowerMenuScript;
+
+    [SerializeField]
+    Transform towerPrefabTransform;
+
+    Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ChooseTower()
+    {
+        towerPrefabTransform.GetComponent<Image>().sprite = GetComponent<Image>().sprite;
+
+
+        Instantiate(towerPrefabTransform, openTowerMenuScript.towerPlaceForPoint);
+
+
+        openTowerMenuScript.gameObject.SetActive(false);
+    }
+}
