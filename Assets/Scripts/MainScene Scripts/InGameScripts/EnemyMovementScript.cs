d8af3fd65b01a04ffb4511b85de75bd430b7c29e
riazonbin <riazonbin@mail.ru>
2023-03-21 11:05:50 +0300

    adding turning

--- Assets/Scripts/MainScene Scripts/InGameScripts/EnemyMovementScript.cs
+++ Assets/Scripts/MainScene Scripts/InGameScripts/EnemyMovementScript.cs
@@ -8,12 +8,20 @@ public class EnemyMovementScript : MonoBehaviour
     [HideInInspector]
     public List<Transform> wayPointsList;
     private Vector3 _target;
-    private int _index = 2;
+    private int _index = 1;
     // Start is called before the first frame update
     void Start()
     {
-        wayPointsList = (GameObject.FindGameObjectWithTag("RoutePoints").GetComponentsInChildren<Transform>()).ToList();
-        _target = wayPointsList[1].position;
+        var routePointsObject = GameObject.FindGameObjectWithTag("RoutePoints");
+
+        var wayPointsRoutesCount = routePointsObject.transform.childCount;
+
+        var route = routePointsObject.transform.GetChild(Random.Range(0, wayPointsRoutesCount));
+
+
+        wayPointsList = route.GetComponentsInChildren<Transform>().ToList();
+
+        _target = wayPointsList[_index].position;
     }
 
     // Update is called once per frame
@@ -26,7 +34,7 @@ public class EnemyMovementScript : MonoBehaviour
     {
         GetComponent<SpriteRenderer>().flipX = transform.position.x - _target.x >= 0;
 
-        transform.position = Vector3.MoveTowards(transform.position, _target, 2 * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _target, 4 * Time.deltaTime);
 
         if (_index + 2 > wayPointsList.Count)
         {
